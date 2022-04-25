4583da00237c5cf7fc8de85df46a3e9065bf80fb
sterapin <sterapin@genians.com>
2022-04-25 09:23:59 +0900

    terra

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0672b175139a0f8f4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
