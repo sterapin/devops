907eea33eed158673ad5da146b106310cf089a14
sterapin <aaoo777@naver.com>
2022-04-28 14:21:10 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,8 +23,17 @@ resource "aws_instance" "app_server" {
   }
 }
 
-resource "aws_security_group" "example" {
-  # ... other configuration ...
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+  
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["222.121.135.254"]
+  }
 
   egress {
     from_port        = 0
@@ -32,5 +41,9 @@ resource "aws_security_group" "example" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  tags = {
+    Name = "allow_ssh"
   }
 }
